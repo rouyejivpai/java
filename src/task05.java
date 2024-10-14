95ca17fd388cdfb37872d13af8f72b0ebdb09da6
rouyejivpai <1533532980@qq.com>
2024-10-14 11:12:01 +0800

    Add files via upload

---
+++ src/task05.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.Scanner;
+
+public class task05 {
+    //输入整数直到输入0
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer (the input ends if it is 0): ");
+        int data = input.nextInt();
+
+        int sum = 0;
+        while (data != 0) {
+            sum += data;
+            System.out.print("Enter an integer (the input ends if it is 0): ");
+            data = input.nextInt();
+        }
+        System.out.println("The sum is " + sum);
+    }
+}
