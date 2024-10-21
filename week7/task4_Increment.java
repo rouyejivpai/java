fa8e2d71082d8669db79a68f2cb26fdd2d271887
rouyejivpai <1533532980@qq.com>
2024-10-21 10:08:31 +0800

    Add files via upload

---
+++ week7/task4_Increment.java
@@ -0,0 +1,16 @@
+package week7;
+
+public class task4_Increment {
+
+public static void main (String[] args) {
+    int x = 1;
+    System.out.println("Before the call, x is " + x);
+
+    increment(x);
+    System.out.println("After the call, x is " + x);
+}
+    public static void increment(int n){
+        n++;
+        System.out.println("n inside the method is " + n);
+    }
+}
