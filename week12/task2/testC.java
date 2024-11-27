db095db0351dc74a9aaafa59006bba186bb743a4
rouyejivpai <1533532980@qq.com>
2024-11-27 15:59:01 +0800

    Add files via upload

---
+++ week12/task2/testC.java
@@ -0,0 +1,9 @@
+package week12.task2;
+
+public class testC {
+    public static void main(String[] args) {
+        C c = new C();
+        //System.out.println(c.x);
+        //System.out.println(c.convert);
+    }
+}
